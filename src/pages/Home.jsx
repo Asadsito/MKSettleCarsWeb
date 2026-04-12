056da0b6da00ef67e981ff17e5c74222f1c4d193
Asads1to <a.muhammadkhalil@lancaster.ac.uk>
2026-04-12 01:49:21 +0100

    adjusted deal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -127,7 +127,7 @@ export default function Home() {
               {
                 number: "02",
                 title: "All included",
-                description: "Insurance, services, MOT and road tax,dash cam all included in the deal."
+                description: "Insurance, services, MOT, road tax, dash cam all included in the deal."
               },
               {
                 number: "03",
